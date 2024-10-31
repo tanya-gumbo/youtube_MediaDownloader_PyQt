d0998f4cb773262cc65baa4b88c4498d97fe63b9
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-10-31 21:14:19 +0200

    Created a class MainWindow which when instantiated will create the main window. Also, added expected methods to be used to the class.

---
+++ User Interface/Frontend/UI.py
@@ -0,0 +1,21 @@
+from PyQt6.QtWidgets import QWidget
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def checkbox_clicked(self):
+        """Ensures the checkboxes remain exclusive and updates the format variable"""
+
+    def download_button_clicked(self):
+        """Executes the download thread when the download button is clicked"""
+        pass
+
+    def add_status_menu_items(self):
+        """Adds the media name and the progress bar to the QListWidget in the main window"""
+        pass
+
+    def update_progress_bar(self):
+        """Updates the progress bar of the download"""
+        pass
+
