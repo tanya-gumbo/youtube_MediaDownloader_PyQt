564970a890094e8349c9a3446aa12c08e966075f
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-01 20:16:53 +0200

    Changed the main folder to create a default folder when the app starts

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from User_Interface.Frontend.UI import MainWindow
+from User_Interface.Frontend.MainApplication.UI import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
+    # create the default download folder when the app starts
+    app.aboutToQuit.connect(MainWindow.create_download_folder_on_startup)
     sys.exit(app.exec())
