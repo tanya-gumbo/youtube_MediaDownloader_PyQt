767d3abfebd942b4b467f231ab5236865fa9a98f
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-01 21:57:25 +0200

    Refactored the update json download path file method to update the download path if called

--- User_Interface/Frontend/SettingsWindow/JSON_file_methods.py
+++ User_Interface/Frontend/SettingsWindow/JSON_file_methods.py
@@ -1,5 +1,25 @@
+import json
+
+
 def read_json_file():
     """Reads from the settings json file"""
 
-def update_json_file():
+def update_json_file_path(file_path):
     """Updates data in the settings json file"""
+    # Read the JSON file
+    try:
+        with open('User_Interface/Frontend/SettingsWindow/app_settings.json', 'r') as file:
+            data = json.load(file)
+    except Exception as e:
+        print(e)
+
+    # Update the values (for example, increment the download counter)
+    data['file_path'] = file_path
+
+    # Write the updated data back to the JSON file
+    try:
+        with open('User_Interface/Frontend/SettingsWindow/app_settings.json', 'w') as file:
+            json.dump(data, file, indent=4)
+    except Exception as z:
+        print(z)
+
