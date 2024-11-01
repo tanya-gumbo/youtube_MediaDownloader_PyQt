3a4e6410b5b8537925f490886645fccb3da019e3
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-01 20:33:52 +0200

    Removed the create folder method from the main.py file

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from User_Interface.Frontend.MainApplication.UI import MainWindow
+from User_Interface.Frontend.SettingsWindow.Settings import Settings
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
-    # create the default download folder when the app starts
-    app.aboutToQuit.connect(MainWindow.create_download_folder_on_startup)
     sys.exit(app.exec())
