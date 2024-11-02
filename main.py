9e76d6c2e0c19b32f9d171d3619c5e2e6e15f9d1
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-02 15:28:59 +0200

    Initialized an instance of the Container, added dependencies to the container and refactored the if loop to implement use of the container

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from User_Interface.Frontend.MainApplication.UI import MainWindow
+from User_Interface.Frontend.MainApplication.main_window import MainWindow
+from User_Interface.Frontend.MainApplication.main_layout import MainLayout
+from User_Interface.Frontend.MainApplication.download_functionality import VideoDownloader
 from User_Interface.Frontend.SettingsWindow.Settings import Settings
+from User_Interface.Frontend.container import Container
+
+container = Container()
+container.register('settings', Settings)
+container.register('vid_downloader', VideoDownloader, container.resolve('settings'))
+# container.register('side_menu', SideMenu, container.resolve('settings'))
+container.register('main_layout', MainLayout, container.resolve('vid_downloader'))
+container.register('main_window', MainWindow, container.resolve('main_layout'))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window = MainWindow()
+    main_window = container.resolve('main_window')
     main_window.show()
     sys.exit(app.exec())
