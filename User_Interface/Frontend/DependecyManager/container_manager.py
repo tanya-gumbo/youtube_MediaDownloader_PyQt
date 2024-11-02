845def4e3026399a73b7a01b1b3a2ffc38206810
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-02 16:45:13 +0200

    Registered the customer_status_menu_items class

--- User_Interface/Frontend/DependecyManager/container_manager.py
+++ User_Interface/Frontend/DependecyManager/container_manager.py
@@ -1,6 +1,6 @@
 from User_Interface.Frontend.DependecyManager.container import Container
 from User_Interface.Frontend.MainApplication.download_functionality import VideoDownloader
-from User_Interface.Frontend.MainApplication.main_layout import MainLayout
+from User_Interface.Frontend.MainApplication.main_layout import MainLayout, CustomStatusMenuItems
 from User_Interface.Frontend.MainApplication.main_window import MainWindow
 from User_Interface.Frontend.SettingsWindow.Settings import Settings
 
@@ -8,6 +8,7 @@ def register_dependencies_in_container():
     container = Container()
     container.register('settings', Settings)
     container.register('vid_downloader', VideoDownloader)
+    container.register('custom_status_menu_items', CustomStatusMenuItems)
     # container.register('side_menu', SideMenu, container.resolve('settings'))
     container.register('main_layout', MainLayout)
     container.register('main_window', MainWindow)
