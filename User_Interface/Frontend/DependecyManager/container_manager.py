db5a9ed2a51964fd60e7176b8ead8b2988aa5572
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-02 16:30:33 +0200

    Refactored container manager to remove any resolve statements when registering classes

---
+++ User_Interface/Frontend/DependecyManager/container_manager.py
@@ -0,0 +1,14 @@
+from User_Interface.Frontend.DependecyManager.container import Container
+from User_Interface.Frontend.MainApplication.download_functionality import VideoDownloader
+from User_Interface.Frontend.MainApplication.main_layout import MainLayout
+from User_Interface.Frontend.MainApplication.main_window import MainWindow
+from User_Interface.Frontend.SettingsWindow.Settings import Settings
+
+def register_dependencies_in_container():
+    container = Container()
+    container.register('settings', Settings)
+    container.register('vid_downloader', VideoDownloader)
+    # container.register('side_menu', SideMenu, container.resolve('settings'))
+    container.register('main_layout', MainLayout)
+    container.register('main_window', MainWindow)
+    return container
