33e4258c5b3ae0f79fb0f6d202d6e7ca4cdda777
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-05 01:51:55 +0200

    No change

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from User_Interface.Frontend.MainApplication.main_window import MainWindow
-from User_Interface.Frontend.MainApplication.main_layout import MainLayout
-from User_Interface.Frontend.MainApplication.download_functionality import VideoDownloader
-from User_Interface.Frontend.SettingsWindow.Settings import Settings
-from User_Interface.Frontend.container import Container
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window = container.resolve('main_window')
+    main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
