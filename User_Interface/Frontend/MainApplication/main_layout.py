c072fa449aeb7834dd5ca70b3022cda12d9e72f6
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-05 01:52:57 +0200

    Refactored the download button clicked method. Added a new start download method

--- User_Interface/Frontend/MainApplication/main_layout.py
+++ User_Interface/Frontend/MainApplication/main_layout.py
@@ -1,15 +1,17 @@
+from PyQt6.QtCore import QThread, QEventLoop, QThreadPool
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QLabel, QVBoxLayout, QCheckBox, QButtonGroup, QPushButton, \
-    QListWidgetItem, QProgressBar, QListWidget
-from User_Interface.Frontend.DependecyManager import container_manager as cont_mnger
+    QListWidgetItem, QProgressBar, QListWidget, QStatusBar
+from imageio.plugins.ffmpeg import download
+
+from User_Interface.Frontend.MainApplication.download_functionality import VideoDownloader, DownloadTask
 
 
 class MainLayout(QWidget):
     def __init__(self):
         super().__init__()
-        self.dependency_manager = cont_mnger.register_dependencies_in_container()
         self.youtube_link_entry = QLineEdit()
         self.link_label = QLabel("Link Entry")
-        self.download_button = QPushButton()
+        self.download_button = QPushButton("Download")
         self.main_layout = QVBoxLayout()
         self.mp3_checkbox = QCheckBox()
         self.mp4_checkbox = QCheckBox()
@@ -17,6 +19,7 @@ class MainLayout(QWidget):
         self.checkbox_button_group = QButtonGroup()
         self.checkbox_buttons_box = QHBoxLayout()
         self.status_menu = QListWidget()
+        self.thread_pool = QThreadPool()
         self.define_ui()
 
     def define_ui(self):
@@ -38,8 +41,8 @@ class MainLayout(QWidget):
         # add checkboxes and download button to main layout
         self.checkbox_buttons_box.addWidget(self.mp3_checkbox)
         self.checkbox_buttons_box.addWidget(self.mp4_checkbox)
-        # self.download_button.clicked.connect(self.download_button_clicked)
-        self.download_button.setText("Download")
+
+        self.download_button.clicked.connect(self.download_button_clicked)
         self.checkbox_buttons_box.addWidget(self.download_button)
         self.main_layout.addLayout(self.checkbox_buttons_box)
 
@@ -56,36 +59,56 @@ class MainLayout(QWidget):
 
     def add_status_menu_items(self):
         """Adds status menu items once a download is started"""
-        status_menu_items = self.dependency_manager.resolve('custom_status_menu_items')
-        self.status_menu.addItem(status_menu_items)
-        self.status_menu.setItemWidget(status_menu_items, status_menu_items.widget)
+        list_item1 = QListWidgetItem()
+        status_menu_items = CustomStatusMenuItems()
+        list_item1.setSizeHint(status_menu_items.sizeHint())
+        self.status_menu.addItem(list_item1)
+        self.status_menu.setItemWidget(list_item1, status_menu_items)
+        return status_menu_items
+
+    def download_button_clicked(self):
+        """Executes the download thread when the download button is clicked"""
+        youtube_link = self.youtube_link_entry.text()
+        self.start_download(youtube_link)
+
+    def start_download(self, youtube_link):
+        print("Download_method called")
+        try:
+            status_menu_items = self.add_status_menu_items()
+            download_thread = VideoDownloader(youtube_link, self.media_format)
+            title = download_thread.get_video_title()
+            status_menu_items.set_video_title(title)
+            download_thread.progress_updated.connect(status_menu_items.update_progress_bar)
+            download_thread.download_finished.connect(status_menu_items.update_status_label)
+            download_task = DownloadTask(download_thread)
+            self.thread_pool.start(download_task)
+        except Exception as e:
+            print(e)
 
 
-
-
-class CustomStatusMenuItems(QListWidgetItem):
+class CustomStatusMenuItems(QWidget):
     def __init__(self):
         super().__init__()
         self.progress_bar = QProgressBar()
-        self.status_label = QLabel("Waiting for download to start...")
+        self.status_label = QLabel()
+        self.status_label.setText("Waiting for download to start...")
         self.video_title = QLabel()
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.video_title)
-        self.layout.addWidget(self.progress_bar)
-        self.layout.addWidget(self.status_label)
-
-        self.widget = QWidget()
-        self.widget.setLayout(self.layout)
-        self.setSizeHint(self.widget.sizeHint())
+        self.custom_layout = QVBoxLayout()
+        self.custom_layout.setSpacing(0)
+        self.custom_layout.addWidget(self.video_title)
+        self.custom_layout.addWidget(self.progress_bar)
+        self.custom_layout.addWidget(self.status_label)
+        self.setLayout(self.custom_layout)
 
     def update_progress_bar(self, progress):
         """Updates the progress bar of the download"""
         self.progress_bar.setValue(progress)
-        self.status_label.setText(f"Downloading... - {progress}%")
+        if self.progress_bar.text() > "0":
+            self.update_status_label("Downloading")
 
-    def handle_download_finished(self, result):
-        self.status_label.setText(result)
+    def update_status_label(self, text):
+        self.status_label.setText(text)
 
     def set_video_title(self, title):
         self.video_title.setText(title)
