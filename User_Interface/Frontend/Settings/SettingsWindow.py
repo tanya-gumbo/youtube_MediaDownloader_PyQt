a4a16537ba587255d58b3954dd214667b479c1dc
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-07 15:24:29 +0200

    Added the methods to be used in the class

--- User_Interface/Frontend/Settings/SettingsWindow.py
+++ User_Interface/Frontend/Settings/SettingsWindow.py
@@ -7,3 +7,22 @@ class SettingsWindow(QDialog):
 
 
     def define_ui(self):
+        """Defines elements of the UI"""
+
+    def create_downloads_pane(self):
+        """Creates the downloads pane to be added to the content pane"""
+
+    def create_user_profile_pane(self):
+        """Creates the user profile pane to be added to the content pane"""
+
+    def display_pane(self, index):
+        """Display the pane in the QStackWidget according to the index passed"""
+
+    def update_button_clicked(self):
+        """Saves any changes made when the update button is clicked"""
+
+    def cancel_button_clicked(self):
+        """Closes the window"""
+
+    def change_download_folder(self):
+        """Changes the download folder where downloads are placed"""
