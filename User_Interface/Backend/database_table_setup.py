f5cf3d6c7aef542fb06d8f5d6361009e44800082
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-14 01:49:20 +0200

    Removed unnecessary import statements

--- User_Interface/Backend/database_table_setup.py
+++ User_Interface/Backend/database_table_setup.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, func, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
