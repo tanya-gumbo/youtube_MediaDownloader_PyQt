e8197f040fe4910827c83edeb5eaf5614f427e0d
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-13 23:15:10 +0200

    Tested all the add layout methods with dummy code

--- User_Interface/Frontend/Settings/DashboardWindow.py
+++ User_Interface/Frontend/Settings/DashboardWindow.py
@@ -1,5 +1,6 @@
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QDialog, QListWidget, QStackedWidget, QVBoxLayout, QHBoxLayout
+from PyQt6.QtWidgets import QDialog, QListWidget, QStackedWidget, QVBoxLayout, QHBoxLayout, QStackedLayout, QPushButton, \
+    QWidget
 
 
 class DashboardWindow(QDialog):
@@ -9,37 +10,67 @@ class DashboardWindow(QDialog):
         self.setWindowIcon(QIcon("User_Interface/Frontend/Settings/window_dash_icon.png"))
         self.setGeometry(100, 100, 600, 500)
         self.options_list = QListWidget()
-        self.content_layout = QStackedWidget()
+        self.content_layout = QStackedLayout()
         self.define_ui()
 
     def define_ui(self):
-        self.options_list.addItem("User profile")
-        self.options_list.addItem("Stats on music")
-        self.options_list.addItem("Music recommendation")
-        self.options_list.setMaximumWidth(150)
-        self.options_list.currentRowChanged(self.select_content_layout)
+        try:
+            self.options_list.addItem("User profile")
+            self.options_list.addItem("Stats on music")
+            self.options_list.addItem("Music recommendation")
+            self.options_list.setMaximumWidth(150)
+            self.options_list.currentRowChanged.connect(self.select_content_layout)
 
-        window_layout = QHBoxLayout()
-        window_layout.addWidget(self.options_list)
-        window_layout.addWidget(self.content_layout)
+            user_profile = self.create_user_profile()
+            stats_on_music = self.create_stats_on_music()
+            music_recommendations = self.create_music_recommendations()
 
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(window_layout)
-        self.setLayout(main_layout)
+            self.content_layout.addWidget(user_profile)
+            self.content_layout.addWidget(stats_on_music)
+            self.content_layout.addWidget(music_recommendations)
+
+            window_layout = QHBoxLayout()
+            window_layout.addWidget(self.options_list)
+            window_layout.addLayout(self.content_layout)
+            main_layout = QVBoxLayout()
+            main_layout.addLayout(window_layout)
+            self.setLayout(main_layout)
+        except Exception as e:
+            traceback = e.__traceback__
+            # Get the line number where the exception occurred
+            line_number = traceback.tb_lineno
+            print("The Dashboard error is", e, "and the line number is ", line_number)
 
     def create_user_profile(self):
         """Create the user account content pane"""
-        pass
+        widget = QWidget()
+        example_layout = QVBoxLayout()
+        one = QPushButton("User layout")
+        example_layout.addWidget(one)
+        widget.setLayout(example_layout)
+        return widget
 
     def create_stats_on_music(self):
         """Create the statistics on music pane"""
-        pass
+        widget = QWidget()
+        example_layout = QVBoxLayout()
+        one = QPushButton("Music stats")
+        example_layout.addWidget(one)
+        widget.setLayout(example_layout)
+        return widget
 
     def create_music_recommendations(self):
         """Create the music recommendations pane"""
-        pass
+        widget = QWidget()
+        example_layout = QVBoxLayout()
+        one = QPushButton("Music Recs")
+        example_layout.addWidget(one)
+        widget.setLayout(example_layout)
+        return widget
 
     def select_content_layout(self, index):
+        count = self.content_layout.count()
+        print('The index is', index, "and the number of children is", count)
         if index == 0:
             self.content_layout.setCurrentIndex(index)
         elif index == 1:
