8291b5589e537e4c6907d81430f22a626c56504b
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-14 22:03:59 +0200

    Made the app execute in an asynchronous manner

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import asyncio
 import sys
+import threading
+
 from PyQt6.QtWidgets import QApplication
+from qasync import QEventLoop
+import uvicorn
+from User_Interface.Backend.fastapi_endpoints import app, start_fastapi
 from User_Interface.Frontend.MainApplication.main_window import MainWindow
 
-if __name__ == "__main__":
+async def main():
     app = QApplication(sys.argv)
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
+    # Start the FastAPI server in a separate thread
+    fastapi_thread = threading.Thread(target=start_fastapi, daemon=True)
+    fastapi_thread.start()
+
     main_window = MainWindow()
     main_window.show()
-    sys.exit(app.exec())
+
+    with loop:
+        sys.exit(loop.run_forever()) # Runs the event loop
+
+if __name__ == "__main__":
+    asyncio.run(main())
