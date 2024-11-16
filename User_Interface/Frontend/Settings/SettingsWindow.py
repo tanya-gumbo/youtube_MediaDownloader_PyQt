9dceafb3ce0890d8282f123db32cd38a8a799236
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-16 17:59:03 +0200

    Added functionality to the update, cancel and change folder buttons

--- User_Interface/Frontend/Settings/SettingsWindow.py
+++ User_Interface/Frontend/Settings/SettingsWindow.py
@@ -1,7 +1,6 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QDialog, QStackedWidget, QListWidget, QHBoxLayout, QVBoxLayout, QPushButton, QWidget, \
-    QLabel, QFormLayout, QLineEdit
+    QLabel, QFormLayout, QLineEdit, QMessageBox, QFileDialog
 from User_Interface.Frontend.Settings import JSON_file_methods as jsn
 
 class SettingsWindow(QDialog):
@@ -9,6 +8,7 @@ class SettingsWindow(QDialog):
         super().__init__()
         self.setGeometry(100, 100, 400, 300)
         self.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose, False)
+        self.download_path = QLabel()
         self.contents_pane = QStackedWidget()
         self.define_ui()
 
@@ -29,7 +29,9 @@ class SettingsWindow(QDialog):
 
             update_and_cancel_button_box = QHBoxLayout()
             update_button = QPushButton("Update")
+            update_button.clicked.connect(self.update_button_clicked)
             cancel_button = QPushButton("Cancel")
+            cancel_button.clicked.connect(self.cancel_button_clicked)
             update_and_cancel_button_box.addWidget(update_button)
             update_and_cancel_button_box.addWidget(cancel_button)
 
@@ -53,15 +55,15 @@ class SettingsWindow(QDialog):
 
         download_label = QLabel()
         download_label.setText("Download folder:")
-        download_path = QLabel()
-        download_path.setText(download_path_text)
+        self.download_path.setText(download_path_text)
         change_path_button = QPushButton("Change download folder")
+        change_path_button.clicked.connect(self.change_download_folder)
         change_path_tool_tip = "Changes the default download folder"
         change_path_button.setToolTip(change_path_tool_tip)
 
         layout = QFormLayout()
         layout.addWidget(download_label)
-        layout.addWidget(download_path)
+        layout.addWidget(self.download_path)
         layout.addWidget(change_path_button)
         main_layout.setLayout(layout)
         return main_layout
@@ -76,9 +78,21 @@ class SettingsWindow(QDialog):
 
     def update_button_clicked(self):
         """Saves any changes made when the update button is clicked"""
+        new_path = self.download_path.text()
+        jsn.update_json_file_path(new_path)
+        QMessageBox.information(
+            self,
+            "Update status",
+            "Default download folder updated"
+        )
 
     def cancel_button_clicked(self):
         """Closes the window"""
+        self.close()
 
     def change_download_folder(self):
         """Changes the download folder where downloads are placed"""
+        default_path = jsn.read_json_file_path()
+        folder_path = QFileDialog.getExistingDirectory(self, directory=default_path)
+        if folder_path:
+            self.download_path.setText(f'Selected Path: {folder_path}')
