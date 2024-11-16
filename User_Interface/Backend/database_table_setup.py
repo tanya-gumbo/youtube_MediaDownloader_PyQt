aa482e45bd98d69ec945401f44606721d4551a09
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-16 17:35:47 +0200

    No changes

--- User_Interface/Backend/database_table_setup.py
+++ User_Interface/Backend/database_table_setup.py
@@ -1,19 +1,14 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, func, Boolean
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, func, Boolean, text
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship, Session
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 class User(Base):
     """Creates a user authentication model"""
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, unique=True, index=True, nullable=False)
+    user_name = Column(String, primary_key=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime, default = func.now())
     max_tokens = Column(Integer, default=3)
@@ -25,7 +20,7 @@ class Download(Base):
     """Creates a download tracking model"""
     __tablename__ = 'downloads'
     download_id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), index=True)
+    user_id = Column(String, ForeignKey('users.user_name'), index=True)
     video_url = Column(String, nullable=False, index=True)
     download_type = Column(String, nullable=False)
     artist_name = Column(String)
@@ -36,15 +31,19 @@ class RecoveryTokens(Base):
     """Creates a users recovery token model"""
     __tablename__ = "recovery_tokens"
     recovery_token_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), index=True)
+    user_id = Column(String, ForeignKey('users.user_name'), index=True)
     hashed_token = Column(String(255),unique=True, nullable=False)
     use_status = Column(Boolean, default=False)
 
 
-#Create the tables
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Create the table
 # Base.metadata.create_all(bind=engine)
 # print("Tables created successfully")
-
 def get_db():
     db = SessionLocal()
     try:
