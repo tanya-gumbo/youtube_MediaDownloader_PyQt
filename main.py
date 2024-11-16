33a188cf7d48690c13e4c3c7447d81fc089efcf8
Tanya Gumbo <tanyagumbo05@gmail.com>
2024-11-16 20:23:54 +0200

    The fastapi app is only started if the user is logged out

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from qasync import QEventLoop
 import uvicorn
 from User_Interface.Backend.fastapi_endpoints import app, start_fastapi
 from User_Interface.Frontend.MainApplication.main_window import MainWindow
+from User_Interface.Frontend.Settings import JSON_file_methods as jsn
 
 async def main():
+    user_login_status = jsn.read_json_status()
     app = QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
-
     # Start the FastAPI server in a separate thread
     fastapi_thread = threading.Thread(target=start_fastapi, daemon=True)
-    fastapi_thread.start()
+    if user_login_status != "logged in":
+        fastapi_thread.start()
+
     main_window = MainWindow()
     main_window.show()
 
